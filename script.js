8cfbb05069a76c66ac5a4e8bf9cc532a20f61030
Magui Brollo <maguieb@hotmail.com>
2024-02-07 08:41:13 -0300

    Funcionalidad del menú.

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+/* ================== Menú   ================ */
+
+
+const menuInicio = document.getElementById("menu-inicio");
+const menuBalance = document.getElementById("menu-balance");
+const menuCategorias = document.getElementById("menu-categorias");
+const menuReportes = document.getElementById("menu-reportes");
+
+
+
+const contenedor_menuInicio = document.getElementById("cont-menu-inicio");
+const contenedor_menuBalance = document.getElementById("cont-menu-balance");
+const contenedor_menuCategorias = document.getElementById(
+	"cont-menu-categorias"
+);
+const contenedor_menuReportes = document.getElementById("cont-menu-reportes");
+
+function mostrar(mostrar) {
+	contenedor_menuInicio.classList.add("hidden");
+	contenedor_menuBalance.classList.add("hidden");
+	contenedor_menuCategorias.classList.add("hidden");
+	contenedor_menuReportes.classList.add("hidden");
+
+	mostrar.classList.remove("hidden");
+}
+
+menuInicio.addEventListener("click", () => {
+	mostrar(contenedor_menuInicio);
+});
+
+menuBalance.addEventListener("click", () => {
+	mostrar(contenedor_menuBalance);
+});
+
+menuCategorias.addEventListener("click", () => {
+	mostrar(contenedor_menuCategorias);
+});
+
+menuReportes.addEventListener("click", () => {
+	mostrar(contenedor_menuReportes);
+});
+
 /* ======================== CLARO - OSCURO ========================  */
 const btn_claro_oscuro = document.getElementById("btn-claro-oscuro");
 
