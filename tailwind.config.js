1ad618d4440b13821ea08927ad991a570a395924
lucbea <ferrerlb@gmail.com>
2024-02-08 21:39:41 -0300

    borre lo de antes y traje lo que hicimos con Magui

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+	darkMode: "class",
+	theme: {
+		screens: {
+			sm: "600px",
+			md: "860px",
+			lg: "1024px",
+		},
+		extend: {
+			fontFamily: {
+				Lora: ['"Lora"'],
+				Raleway: ['"Raleway"'],
+			},
+			colors: {
+				colPpal: "#A3C3F2",
+				colSec: "#3F5A9F",
+			},
+		},
+	},
+};
