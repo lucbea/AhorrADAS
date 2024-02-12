b8ae6c3324dc0bb541f65250ce091aa461ca5513
Magui Brollo <maguieb@hotmail.com>
2024-02-12 09:29:59 -0300

    Menú Reportes: COMPLETO. FavIcon.

--- scriptReportes.js
+++ scriptReportes.js
@@ -2,131 +2,41 @@
 const l_categorias = [
 	{
 		id: "c377b47a-f80c-4be5-8f9d-ead89cbb0a4a",
-		nombre: "Comida Italiana",
+		nombre: "COMIDA ITALIANA",
 	},
 	{
 		id: "42948a68-efd1-4bc2-932c-4a8539823c54",
-		nombre: "Servicios",
+		nombre: "SERVICIOS",
 	},
 	{
 		id: "ac295847-93c5-4fc8-b4ab-ecda5e8f4bb0",
-		nombre: "Salidas",
+		nombre: "SALIDAS",
 	},
 	{
 		id: "de2a87a2-5eb4-4fe3-a300-d95a33b0e777",
-		nombre: "Educación",
+		nombre: "EDUCACIÓN",
 	},
 	{
 		id: "33bded42-a463-4dd1-a0ee-c115597e7f37",
-		nombre: "Transporte",
+		nombre: "TRANSPORTE",
 	},
 	{
 		id: "b6d68d8a-8036-4115-883b-5cb57c1c3813",
-		nombre: "Trabajo Alocado diario",
+		nombre: "TRABAJO DIARIO ALOCADO",
 	},
 	{
 		id: "e989043c-7d70-4e22-9534-0b3ccdf83be3",
-		nombre: "Alquiler",
+		nombre: "ALQUILER DE LA CASA",
 	},
 ];
 
-const l_operaciones = [
-	{
-		id: "da576431-7aae-4d1a-8b19-aeeeceef9509",
-		descripcion: "compra",
-		monto: 123,
-		categoria: "c377b47a-f80c-4be5-8f9d-ead89cbb0a4a",
-		tipo: "GASTO",
-		fecha: "2024/02/07",
-	},
-	{
-		id: "62124484-c39d-4663-8cbb-0e7a3a9e856b",
-		descripcion: "pizza",
-		monto: 5000,
-		categoria: "c377b47a-f80c-4be5-8f9d-ead89cbb0a4a",
-		tipo: "GASTO",
-		fecha: "2024/02/07",
-	},
-	{
-		id: "fc5199e7-89ad-46a9-8be2-153df86a13e4",
-		descripcion: "aguinaldo",
-		monto: 1000000,
-		categoria: "b6d68d8a-8036-4115-883b-5cb57c1c3813",
-		tipo: "GANANCIA",
-		fecha: "2024/02/07",
-	},
-	{
-		id: "2d50aad3-f585-4bd2-88f4-cb2bdb66f021",
-		descripcion: "Cena lunes",
-		monto: 50000,
-		categoria: "c377b47a-f80c-4be5-8f9d-ead89cbb0a4a",
-		tipo: "GASTO",
-		fecha: "2024/02/05",
-	},
-	{
-		id: "2959f1ea-368f-4d4b-8151-73ce497f56cc",
-		descripcion: "Merienda",
-		monto: 40000,
-		categoria: "c377b47a-f80c-4be5-8f9d-ead89cbb0a4a",
-		tipo: "GASTO",
-		fecha: "2024/02/06",
-	},
-	{
-		id: "01dc7555-b874-490f-9d03-e7a61d441efb",
-		descripcion: "Librería",
-		monto: 10000,
-		categoria: "de2a87a2-5eb4-4fe3-a300-d95a33b0e777",
-		tipo: "GASTO",
-		fecha: "2024/02/02",
-	},
-	{
-		id: "5bc4c88c-c6e5-4d18-bbc9-cd6a5977a73f",
-		descripcion: "Aguinaldo",
-		monto: 50000,
-		categoria: "b6d68d8a-8036-4115-883b-5cb57c1c3813",
-		tipo: "GANANCIA",
-		fecha: "2024/01/31",
-	},
-	{
-		id: "99ccefa4-ff57-4e4a-a178-9e35965d5417",
-		descripcion: "Venta net",
-		monto: 40000,
-		categoria: "b6d68d8a-8036-4115-883b-5cb57c1c3813",
-		tipo: "GANANCIA",
-		fecha: "2024/02/08",
-	},
-	{
-		id: "89965aab-991a-44ad-9ded-ff0731d7da83",
-		descripcion: "Sueldo",
-		monto: 80000,
-		categoria: "e989043c-7d70-4e22-9534-0b3ccdf83be3",
-		tipo: "GANANCIA",
-		fecha: "2024/02/08",
-	},
-	{
-		id: "4c121508-97a7-4dc7-ba6d-858794f5529f",
-		descripcion: "Alquiler",
-		monto: 60000,
-		categoria: "42948a68-efd1-4bc2-932c-4a8539823c54",
-		tipo: "GASTO",
-		fecha: "2024/01/30",
-	},
-	{
-		id: "c4113266-375a-4b26-8a25-4d2040433291",
-		descripcion: "Remedios",
-		monto: 20000,
-		categoria: "42948a68-efd1-4bc2-932c-4a8539823c54",
-		tipo: "GASTO",
-		fecha: "2024/02/07",
-	},
-];
+const l_operaciones = [];
 
 localStorage.setItem("categorias", JSON.stringify(l_categorias));
 localStorage.setItem("operaciones", JSON.stringify(l_operaciones));
 
 /* ******* HASTA AQUI DSPS BORRRAAAAAAAAARRRRRRRRRRRRRR */
-
-/* ------------------------------------------------ */
+/* =============================================================== */
 /* "Categoría" con Mayor GANANCIA y con Mayor GASTO */
 function CatMayorGananciaGasto(tipo, mayor) {
 	categorias.forEach((cat) => {
@@ -147,7 +57,7 @@ function CatMayorGananciaGasto(tipo, mayor) {
 	});
 }
 
-/* ------------------------------------------------ */
+/* =============================================================== */
 /* Categoría con Mayor BALANCE */
 function CatMayorBalance(mayor) {
 	categorias.forEach((cat) => {
@@ -170,7 +80,7 @@ function CatMayorBalance(mayor) {
 	});
 }
 
-/* ------------------------------------------------ */
+/* =============================================================== */
 /* "MES" con Mayor GANANCIA y con Mayor GASTO */
 function MesMayorGananciaGasto(tipo, mayor) {
 	let arrayAnioMes = [];
@@ -211,6 +121,82 @@ function MesMayorGananciaGasto(tipo, mayor) {
 	});
 }
 
+/* =============================================================== */
+/*  TOTALES por CATEGORIA (Ganancia-Gasto-Balance) */
+function totalesPorCat(totales) {
+	categorias.forEach((cat) => {
+		const filtrarOperPorCat = operaciones.filter(
+			(oper) => cat.id === oper.categoria
+		);
+
+		let gan = 0;
+		let gas = 0;
+		filtrarOperPorCat.forEach((fil) => {
+			fil.tipo === "GANANCIA" ? (gan += fil.monto) : (gas += fil.monto);
+		});
+
+		if (gan > 0 || gas > 0) {
+			totales.push({
+				nombreCat: cat.nombre,
+				ganancia: gan,
+				gasto: gas,
+				balance: gan - gas,
+			});
+		}
+	});
+}
+
+/* ===================================================== */
+/*  TOTALES por MESES (Ganancia-Gasto-Balance) */
+function totalesPorMes(totales) {
+	let arrayAnioMes = [];
+	operaciones.forEach((oper) => {
+		const fechaOper = new Date(oper.fecha);
+		const anio = fechaOper.getFullYear();
+		const mes = fechaOper.getMonth();
+
+		if (!arrayAnioMes.some((e) => anio === e.anio && mes === e.mes)) {
+			arrayAnioMes.push({
+				anio: fechaOper.getFullYear(),
+				mes: fechaOper.getMonth(),
+			});
+		}
+	});
+
+	arrayAnioMes.forEach((anioMes) => {
+		const filtrarOperPorMes = operaciones.filter(
+			(oper) =>
+				new Date(oper.fecha).getFullYear() === anioMes.anio &&
+				new Date(oper.fecha).getMonth() === anioMes.mes
+		);
+
+		let gan = 0;
+		let gas = 0;
+		filtrarOperPorMes.forEach((fil) => {
+			fil.tipo === "GANANCIA" ? (gan += fil.monto) : (gas += fil.monto);
+		});
+
+		if (gan > 0 || gas > 0) {
+			totales.push({
+				mes: `${anioMes.anio} - ${meses[anioMes.mes]}`,
+				mesNum: anioMes.mes,
+				anio: anioMes.anio,
+				ganancia: gan,
+				gasto: gas,
+				balance: gan - gas,
+			});
+		}
+	});
+}
+/* ===================================================== */
+
+function formatPesos(num) {
+	return num.toLocaleString("es-ES", {
+		minimumFractionDigits: 2,
+		maximumFractionDigits: 2,
+	});
+}
+
 /* ======================== MOSTRAR CON/SIN REPORTES ========================  */
 /* Mostrar Imagen, y texto SIN REPORTES */
 const cont_sin_reporte = document.getElementById("cont-sin-reporte");
@@ -228,21 +214,24 @@ const mes_may_gana_imp = document.getElementById("mes-may-gana-imp");
 const mes_may_gast_nom = document.getElementById("mes-may-gast-nom");
 const mes_may_gast_imp = document.getElementById("mes-may-gast-imp");
 
+const totales_por_categoria = document.getElementById("totales-por-categoria");
+const totales_por_mes = document.getElementById("totales-por-mes");
+
 let categorias = [];
 let operaciones = [];
 const meses = [
-	/* Meses para Mes CON MAYOR ganancia */ "ENERO",
-	"FEBRERO",
-	"MARZO",
-	"ABRIL",
-	"MAYO",
-	"JUNIO",
-	"JULIO",
-	"AGOSTO",
-	"SEPTIEMBRE",
-	"OCTUBRE",
-	"NOVIEMBRE",
-	"DICIEMBRE",
+	"ENE",
+	"FEB",
+	"MAR",
+	"ABR",
+	"MAY",
+	"JUN",
+	"JUL",
+	"AGO",
+	"SEP",
+	"OCT",
+	"NOV",
+	"DIC",
 ];
 
 /* ------------------------------------------------ */
@@ -260,16 +249,16 @@ function mostrarConReportes() {
 	categorias = JSON.parse(localStorage.getItem("categorias"));
 	operaciones = JSON.parse(localStorage.getItem("operaciones"));
 
+	/* -------------------------- */
 	/*  CATEGORIA CON MAYOR GANANCIA */
 	let mayor = {
 		nombreCategoria: "",
 		importeCategoria: 0,
 	};
+
 	CatMayorGananciaGasto("GANANCIA", mayor);
 	cat_may_gana_nom.innerHTML = `${mayor.nombreCategoria}`;
-	cat_may_gana_imp.innerHTML = `$: ${mayor.importeCategoria.toLocaleString(
-		"es-ES"
-	)}`;
+	cat_may_gana_imp.innerHTML = `$${formatPesos(mayor.importeCategoria)}`;
 
 	/* -------------------------- */
 	/*  CATEGORIA CON MAYOR GASTO */
@@ -279,9 +268,7 @@ function mostrarConReportes() {
 	};
 	CatMayorGananciaGasto("GASTO", mayor);
 	cat_may_gast_nom.innerHTML = `${mayor.nombreCategoria}`;
-	cat_may_gast_imp.innerHTML = `-$ ${mayor.importeCategoria.toLocaleString(
-		"es-ES"
-	)}`;
+	cat_may_gast_imp.innerHTML = `-$${formatPesos(mayor.importeCategoria)}`;
 
 	/* -------------------------- */
 	/*  CATEGORIA CON MAYOR BALANCE */
@@ -291,9 +278,7 @@ function mostrarConReportes() {
 	};
 	CatMayorBalance(mayor);
 	cat_may_bala_nom.innerHTML = `${mayor.nombreCategoria}`;
-	cat_may_bala_imp.innerHTML = `$: ${mayor.importeCategoria.toLocaleString(
-		"es-ES"
-	)}`;
+	cat_may_bala_imp.innerHTML = `$${formatPesos(mayor.importeCategoria)}`;
 
 	/* -------------------------- */
 	/*  Mes CON MAYOR GANANCIA */
@@ -303,9 +288,7 @@ function mostrarConReportes() {
 	};
 	MesMayorGananciaGasto("GANANCIA", mayorAnioMes);
 	mes_may_gana_nom.innerHTML = `${mayorAnioMes.mes}`;
-	mes_may_gana_imp.innerHTML = `$: ${mayorAnioMes.importe.toLocaleString(
-		"es-ES"
-	)}`;
+	mes_may_gana_imp.innerHTML = `$${formatPesos(mayorAnioMes.importe)}`;
 
 	/* -------------------------- */
 	/*  Mes CON MAYOR GASTO */
@@ -315,16 +298,114 @@ function mostrarConReportes() {
 	};
 	MesMayorGananciaGasto("GASTO", mayorAnioMes);
 	mes_may_gast_nom.innerHTML = `${mayorAnioMes.mes}`;
-	mes_may_gast_imp.innerHTML = `-$ ${mayorAnioMes.importe.toLocaleString(
-		"es-ES"
-	)}`;
+	mes_may_gast_imp.innerHTML = `-$${formatPesos(mayorAnioMes.importe)}`;
+
+	/* -------------------------- */
+	/* Totales por Categoria  */
+	let totalesCat = [];
+	totalesPorCat(totalesCat);
+	totalesCat.sort((a, b) => {
+		return a.nombreCat.localeCompare(b.nombreCat);
+	});
+
+	totalesCat.forEach((totCat) => {
+		let x;
+		if (totCat.balance > 0) {
+			x = `<div class="w-full sm:w-[33%] flex justify-end"> $${formatPesos(
+				totCat.balance
+			)} </div>`;
+		} else {
+			x = `<div class="w-full sm:w-[33%] flex justify-end text-[red] dark:text-red-900">
+			-$${formatPesos(Math.abs(totCat.balance))} </div>`;
+		}
+
+		totales_por_categoria.innerHTML += `
+		<div class="w-full flex flex-row border-t-2 border-indigo-100 dark:border-gray-400" > 
+			  <div class="w-[35%] sm:w-[30%] md:w-[40%] ">
+							${totCat.nombreCat}
+				</div>
+
+				<div class="w-[65%] sm:w-[70%] md:w-[60%] flex flex-col sm:flex-row">
+					<div class="w-full sm:w-[33%] flex justify-end">
+						$${formatPesos(totCat.ganancia)}
+					</div>
+
+					<div
+						class="w-full sm:w-[33%] flex justify-end text-[red] dark:text-red-900"
+					>
+						-$${formatPesos(totCat.gasto)}
+					</div> 
+					${x}
+				</div> 
+		</div>`;
+	});
+
+	/* -------------------------- */
+	/* Totales por MESES  */
+	let totalesMes = [];
+	totalesPorMes(totalesMes);
+	totalesMes.sort((a, b) => {
+		if (a.anio > b.anio) {
+			return -1;
+		} else {
+			if (a.anio < b.anio) {
+				return 1;
+			} else {
+				if (a.mesNum > b.mesNum) {
+					return -1;
+				} else {
+					if (a.mesNum < b.mesNum) {
+						return 1;
+					} else {
+						return 0;
+					}
+				}
+			}
+		}
+	});
+
+	totalesMes.forEach((totMes) => {
+		let x;
+		if (totMes.balance > 0) {
+			x = `<div class="w-full sm:w-[33%] flex justify-end"> $${formatPesos(
+				totMes.balance
+			)} </div>`;
+		} else {
+			x = `<div class="w-full sm:w-[33%] flex justify-end text-[red] dark:text-red-900">
+			-$${formatPesos(Math.abs(totMes.balance))} </div>`;
+		}
+
+		totales_por_mes.innerHTML += `
+		<div class="w-full flex flex-row border-t-2 border-indigo-100 dark:border-gray-400" > 
+			  <div class="w-[35%] sm:w-[30%] md:w-[40%] ">
+							${totMes.mes}
+				</div>
+
+				<div class="w-[65%] sm:w-[70%] md:w-[60%] flex flex-col sm:flex-row">
+					<div class="w-full sm:w-[33%] flex justify-end">
+						$${formatPesos(totMes.ganancia)}
+					</div>
+
+					<div
+						class="w-full sm:w-[33%] flex justify-end text-[red] dark:text-red-900"
+					>
+						-$${formatPesos(totMes.gasto)}
+					</div> 
+					${x}
+				</div> 
+		</div>`;
+	});
 }
 
+/* ------------------------------------------------ */
 /* ------------------------------------------------ */
 /* viene de SCRIPT.JS */
 function mostrarReportes() {
 	if (localStorage.getItem("operaciones") !== null) {
-		mostrarConReportes();
+		const lon = JSON.parse(localStorage.getItem("operaciones")).length;
+		if (lon > 0) {
+			mostrarConReportes();
+		}
 	} else {
 		mostrarSinReportes();
 	}
